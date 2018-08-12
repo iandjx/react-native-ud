269ad7b62c3f63b198337cec79d539bbc06cb9b8
ian de jesus <ian.dejesus.md@gmail.com>
2018-08-12 21:54:50 +0900

    Rendered AlbumList state

--- src/components/albumList/AlbumList.js
+++ src/components/albumList/AlbumList.js
@@ -10,11 +10,18 @@ class AlbumList extends React.Component {
         axios.get('http://rallycoding.herokuapp.com/api/music_albums')
         .then(response => this.setState({albums: response.data}));
     }
+
+    renderAlbums(){
+        return this.state.albums.map(
+            album => <Text>{album.title}</Text>
+        );
+    }
+
     render(){
         console.log(this.state);
         return(
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
