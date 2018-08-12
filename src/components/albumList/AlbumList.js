bc32db4cf6d3e5b615c02f66d594d2c78e011392
ian de jesus <ian.dejesus.md@gmail.com>
2018-08-12 17:44:30 +0900

    Converted AlbumList to as class based component

--- src/components/albumList/AlbumList.js
+++ src/components/albumList/AlbumList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const AlbumList = () => {
-    return(
-        <View>
+class AlbumList extends React.Component {
+    render(){
+        return(
+            <View>
             <Text>Album List!!!</Text>
         </View>
-    );
-};
+        );
+    }
+}
+
 
 export default AlbumList;
