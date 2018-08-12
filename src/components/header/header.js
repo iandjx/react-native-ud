0da0ea935e1a40225627b467a29db8ac7ee303fb
ian de jesus <ian.dejesus.md@gmail.com>
2018-08-12 16:52:18 +0900

    Added Header styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 const Header = () => {
-    return <Text>Albums!</Text>
+    return(
+        <View style={styles.viewStyle}>
+            <Text style={styles.headerStyle}>Albums!</Text>
+        </View>
+
+    )
+   
 };
+
+const styles = StyleSheet.create({
+    headerStyle: {
+        fontSize: 20
+    },
+    viewStyle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F8F8F8',
+        width: "100%",
+        height: 60,
+        padding: 15,
+        shadowColor: 'black',
+        shadowOffset: {width:0, height:2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    }
+  });
 
 export default Header;
