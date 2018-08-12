a51145f7b4a5e105b30bb580af30cb0f7494cfc0
ian de jesus <ian.dejesus.md@gmail.com>
2018-08-12 21:35:08 +0900

    Update AlbumList state on HTTP get response

--- src/components/albumList/AlbumList.js
+++ src/components/albumList/AlbumList.js
@@ -3,16 +3,19 @@ import {View, Text} from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends React.Component {
+
+    state = {albums: []};
     
     componentDidMount(){
         axios.get('http://rallycoding.herokuapp.com/api/music_albums')
-        .then(response => console.log(response));
+        .then(response => this.setState({albums: response.data}));
     }
     render(){
+        console.log(this.state);
         return(
             <View>
-            <Text>Album List!!!</Text>
-        </View>
+                <Text>Album List!!!</Text>
+            </View>
         );
     }
 }
