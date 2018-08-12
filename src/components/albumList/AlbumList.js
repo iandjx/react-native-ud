0c01293e3db49bcf3b8851a5b0b6aeac4a3727d4
ian de jesus <ian.dejesus.md@gmail.com>
2018-08-12 22:24:10 +0900

    Added AlbumDetail Component
    AlbumDetail component takes in the prop from AlbumList to get Album.title and render it

--- src/components/albumList/AlbumList.js
+++ src/components/albumList/AlbumList.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from '../albumDetail/AlbumDetail'
 
 class AlbumList extends React.Component {
 
@@ -12,8 +14,8 @@ class AlbumList extends React.Component {
     }
 
     renderAlbums(){
-        return this.state.albums.map(
-            album => <Text>{album.title}</Text>
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
         );
     }
 
