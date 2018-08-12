03085fe66b66232cc1db7a05e15fa3d164d5595e
ian de jesus <ian.dejesus.md@gmail.com>
2018-08-12 17:41:29 +0900

    Added AlbumList Component

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Header from './src/components/header/header'
+import Header from './src/components/header/Header'
+import AlbumList from './src/components/albumList/AlbumList'
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Header headerText={'Albums'}/>
+        <AlbumList/>
       </View>
     );
   }
